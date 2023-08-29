98ccc2996c45ec616e7462955e18f755978e9366
barbaraarruda <barbarahmiley@gmail.com>
2023-08-29 15:08:36 -0300

    minutagem 33:39

---
+++ client/src/components/CustomButton.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const CustomButton = ({ type, title, customStyles, handleClick }) => {
+    const generateStyle = (type) => {
+        
+    }
+    return (
+        <button 
+        className={'px-2 py-1.5 flex-1 rounded-md ${customStyles}'}
+        style={generateStyle(type)}
+        >
+            {title}
+        </button>
+    )
+}
+
+export default CustomButton
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { motion, AnimationPresence, AnimatePresence } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { useSnapshot } from 'valtio';
 import state from '../store';
 import { 
@@ -7,6 +7,7 @@ import {
   headTextAnimation,
   slideAnimation
 } from '../config/motion';
+import { CustomButton } from '../components';
 
 const Home = () => {
   const snap = useSnapshot(state);
@@ -15,13 +16,38 @@ const Home = () => {
     <AnimatePresence>
       {snap.intro && (
         <motion.section className="home" {...slideAnimation('left')}>
-          <motion.header>
+          <motion.header {...slideAnimation("down")}>
             <img 
               src='./threejs.png' 
               alt="threejs logo"
               className="w-8 h-8 object-contain"
             />
           </motion.header>
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                VAMOS <br className="xl:block hidden"/> FAZER ISSO.
+              </h1>
+            </motion.div>
+            <motion.div 
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+            />
+           
+            <motion.div>
+              <p className="max-w-md font-normal text-gray-600 text-base">
+                Crie sua exclusiva e única camiseta com a nossa
+                ferramenta de personalização 3D. <strong>DÊ ASAS A SUA IMAGINAÇÃO</strong>{" "}
+                e crie a camiseta que você sempre quis.
+              </p>
+              <CustomButton
+                type="filled"
+                title="Customize It"
+                handleClick={() => state.intro = false}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
+            </motion.div>
+          </motion.div>
         </motion.section>
       )}
     </AnimatePresence>
